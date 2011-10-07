a5a8676ef141cc11940782de655af61cf6ea2226
Steve Jackson <steven.j.jackson@gmail.com>
2011-10-07 06:41:31 -0400

    Lots of functionality, lightboxing

--- static/javascripts/flow.js
+++ static/javascripts/flow.js
@@ -1,17 +1,80 @@
+
 $(document).ready(function() {
-  $(window).scrollTo('1000px', 2000, { axis: 'y' });
+
+  loadGallery();
+
+  $("#lightbox_overlay").click(function() {
+    hideLightbox();
+  });
+
 });
 
 
 // When they scroll to the bottom of the page...
 $(window).scroll(function() {
-  if($(window).scrollTop() == $(document).height() - $(window).height()) {
+  if($(window).scrollTop() >= $(document).height() - $(window).height()) {
     getNextPage();
   }
 });
 
 function getNextPage() {
   $.get('/getNextPage', function(data) {
-    $('.content_container').append(data);
+    $('#content_container').append(data);
+    loadGallery();
   });
+};
+
+function loadGallery() {
+  $('.gallery-image').click(function() {
+    $(window).scrollTo( $(this).offset().top - 65, 500 );
+
+    // grab the full image, stick it into the lightbox
+    showLightbox($(this).position().left, $(this).position().top);
+    $('#lightbox').html($(this).children('.full').html());
+    $('#lightbox .full').css('display', 'block');
+
+    var afterLoaded = function() {
+      var width = $('#lightbox img').width();
+      var height = $('#lightbox img').height();
+      
+      $('#lightbox').animate({
+        'left': '50%',
+        'top': '50%',
+        'margin-top': -height / 2,
+        'margin-left': -width / 2,
+        'width': width,
+        'height': height
+      }, 1000);
+    };
+
+    $('#lightbox').preloader({'ondone':afterLoaded});
+
+  });
+
+  $('.gallery-page:last .thumb').preloader();
+};
+
+function showLightbox(x, y) {
+  $('#content_container').animate({
+    'opacity': 0.2
+  }, 500);
+
+  $('#lightbox').css({
+    'left': x,
+    'top': y,
+    'width': '280px',
+    'height': '280px'
+  });
+
+  $('#lightbox').show();
+  $('#lightbox_overlay').show();
+};
+
+function hideLightbox() {
+  $('#content_container').animate({
+    'opacity': 1.0
+  }, 500);
+
+  $('#lightbox').hide();
+  $('#lightbox_overlay').hide();
 };
---
+++ static/javascripts/jquery.preloader.js
@@ -0,0 +1,60 @@
+// JavaScript Document
+$.fn.preloader = function(options){
+
+    var defaults = {
+                     delay:75,
+                     check_timer:300,
+                     ondone:function(){ },
+                     oneachload:function(image){  },
+                     fadein:500
+                    };
+
+    // variables declaration and precaching images and parent container
+     var options = $.extend(defaults, options),
+     root = $(this) , images = root.find("img").css({"visibility":"hidden",opacity:0}) ,  timer ,  counter = 0, i=0 , checkFlag = [] , delaySum = options.delay;
+
+     init = function(){
+
+       timer = setInterval(function(){
+
+          if(counter>=checkFlag.length)
+          {
+            clearInterval(timer);
+            options.ondone();
+            return;
+          }
+
+          for(i=0;i<images.length;i++)
+          {
+              if(images[i].complete==true)
+              {
+                  if(checkFlag[i]==false)
+                  {
+                      checkFlag[i] = true;
+                      options.oneachload(images[i]);
+                      counter++;
+
+                      delaySum = delaySum + options.delay;
+                  }
+
+                  $(images[i]).css("visibility","visible").delay(delaySum).animate({opacity:1},options.fadein,
+                  function(){ $(this).parent().removeClass("preloader");   });
+              }
+          }
+
+        },options.check_timer)
+    };
+
+    images.each(function(){
+        checkFlag[i++] = false;
+    });
+
+    images = $.makeArray(images);
+
+    timer = setInterval(function(){
+            clearInterval(timer);
+            init();
+            return;
+        },100);
+
+    }
